a0e4f8dc24f25c0e7a0a81555db963eea152177e
Emilie-Rose <78940316+Emilie-Rose@users.noreply.github.com>
2021-07-06 12:06:57 +0200

    mise à jour

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+$serveur="localhost";
+$login="root";
+$pass="";
+
+try{
+    $connexion= new PDO ("mysql:host=$serveur;dbname=webdevelopment",$login,$pass);//j'utilise les bloc try and catch pour la gestion des erreurs
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //exercice1:
+    // "Dans la table languages, supprimer toutes les lignes parlant de Javascript"
+
+    $sup="DELETE FROM languages WHERE language = 'Javascript'";
+    $requette=$connexion->prepare($sup);
+    $requette->execute();
+
+    //exercice2:
+    // "Dans la table frameworks, modifier toutes les lignes ayant le framework REACT par Angular"
+
+    // $modif="UPDATE framworks SET framwork='Angular' WHERE framwork='React' ";//mettre à jour l'age de la personne qui porte l'id 1 en utilisant le mot clé WHERE 
+    // $requette=$connexion->prepare($modif);
+    // $requette->execute();
+
+    // exercice3:
+    // "Dans la table frameworks, afficher toutes les données ayant une version 16.x (x étant un numéro quelconque)"
+
+    // $requette=$connexion->prepare("SELECT * FROM framworks WHERE version>16");
+    // $requette->execute();
+    // $resultat=$requette->fetchAll();//on va stocker le résultat dans une variable pour l'afficher facilement
+  
+    //exercice 4:
+    // "Dans la table frameworks, afficher les lignes ayant pour id 3 et 5"
+
+    // $requette1=$connexion->prepare("SELECT * FROM framworks WHERE id= 3 OR id=5");
+    // $requette1->execute();
+    // $resultat1=$requette1->fetchAll();
+
+    // exercice 5:
+    // "Dans la table languages, modifier la ligne du langage PHP version 6 par la version 6.1"
+
+    // $modif="UPDATE languages SET version=6.1 WHERE language='PHP' AND version=6 ";//mettre à jour l'age de la personne qui porte l'id 1 en utilisant le mot clé WHERE 
+    // $requette2=$connexion->prepare($modif);
+    // $requette2->execute();
+    
+    echo "connexion à la base de donnée webdevelopment est reussi<br>";
+    // echo "les lignes Javascript sont supp de la table languages<br>";
+    // echo "changement de framwork de React à Angular<br> ";
+    // echo "affichage de framwork avec version 16 et plus est reussi <br> ";
+    // echo "changement de version dePHP6 à reussi<br> ";
+    // echo "<h3 style='color:red;'>exercice 3</h3><br>";
+    // echo "<pre>";
+    // print_r($resultat);
+    // echo "</pre>";
+    // echo "<h3 style='color:red;'>exercice 4</h3><br>";
+    // echo "<pre>";
+    // print_r($resultat1);
+    // echo "</pre>";
+}catch(PDOException $e){
+    echo'echec : ' . $e->getMessage();
+}
+?>
